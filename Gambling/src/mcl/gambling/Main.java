8cb57617ecb73e2498deeb4b2ce84e43a05c0996
MclGaming <meowclewizlon@gmail.com>
2024-08-08 20:03:37 -0700

    Save

--- Gambling/src/mcl/gambling/Main.java
+++ Gambling/src/mcl/gambling/Main.java
@@ -7,15 +7,20 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
+import java.util.UUID;
 
 public class Main extends JavaPlugin implements Listener
 {
     Random rnd = new Random();
+    Set<UUID> gamers = new HashSet<>();
 
     @Override
     public void onEnable()
@@ -23,6 +28,18 @@ public class Main extends JavaPlugin implements Listener
         getServer().getPluginManager().registerEvents(this, this);
         PluginCommand cmd = this.getCommand("gamble");
         if (cmd != null) cmd.setExecutor(new GambleExecutor());
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e)
+    {
+        Player p = e.getPlayer();
+        UUID id = p.getUniqueId();
+        if (!gamers.contains(id))
+        {
+            p.getWorld().dropItem(p.getLocation(), new ItemStack(Material.EMERALD));
+            gamers.add(id);
+        }
     }
 
     class GambleExecutor implements CommandExecutor
@@ -36,16 +53,22 @@ public class Main extends JavaPlugin implements Listener
                 ItemStack item = p.getInventory().getItemInMainHand();
                 if (item.getType() == Material.EMERALD)
                 {
-                    int x = item.getAmount() - 1;
-                    if (x < 0) return false;
-                    item.setAmount(x);
-                    p.playSound(p, Sound.ITEM_BUNDLE_DROP_CONTENTS, 1.0f, 1.0f);
-                    Material[] materials = Material.values();
-                    int matIndex = rnd.nextInt(0, materials.length);
-                    Material mat = materials[matIndex];
-                    p.getWorld().dropItemNaturally(p.getLocation(), new ItemStack(mat));
-                    p.playSound(p, Sound.ITEM_BUNDLE_DROP_CONTENTS, 1.0f, 1.0f);
+                    ItemStack offhand = p.getInventory().getItemInOffHand();
+                    if (offhand.getType() == Material.AIR)
+                    {
+                        int x = item.getAmount() - 1;
+                        if (x < 0) return false;
+                        item.setAmount(x);
+                        Material[] materials = Material.values();
+                        int matIndex = rnd.nextInt(0, materials.length);
+                        Material mat = materials[matIndex];
+                        int num = rnd.nextInt(0, mat.getMaxStackSize());
+                        p.getInventory().setItemInOffHand(new ItemStack(mat, num));
+                        p.getWorld().playSound(p, Sound.ITEM_GOAT_HORN_SOUND_1, 1000.0f, 1.0f);
+                    }
+                    else p.sendMessage("Your offhand must be empty");
                 }
+                else p.sendMessage("You must be holding an emerald");
             }
             else sender.sendMessage("Must be player");
 
